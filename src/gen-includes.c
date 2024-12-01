bd30fca732acd8128e0713af876881567d0ae66d
Yury Moskov <aerobus.778@gmail.com>
2024-12-01 23:14:47 +0300

    update src/gen-includes.c

--- src/gen-includes.c
+++ src/gen-includes.c
@@ -10,7 +10,7 @@ void metadata_query(void* ptr);
 #include <string.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv, char** envp)
 {
 	if (metadata_version() == -1)
 	{
@@ -19,6 +19,21 @@ int main()
 	}
 
 	void* target = (void*) 1;
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "1") == 0)
+		{
+			target = (void*) 1; // create includes directories and write headers file
+		}
+		else if (strcmp(argv[1], "2") == 0)
+		{
+			target = (void*) 2; // output all header files
+		}
+		else
+		{
+			target = (void*) argv[1]; // show specified module, e.g. nanoc.std.stdio
+		}
+	}
 	metadata_query(target);
 	return 0;
 }
